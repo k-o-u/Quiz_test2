43b21c109682739bdebf9126f65d109851b0b5dc
k_t <ktwmekcm@gmail.com>
2021-05-05 22:15:57 +0900

    add & modify

--- app/src/main/java/com/websarva/wings/android/quiz_test2/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/quiz_test2/MainActivity.java
@@ -2,6 +2,7 @@ package com.websarva.wings.android.quiz_test2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,24 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lvAnswer = findViewById(R.id.lvAnswer);
-        List<String> answerList = new ArrayList<>();
-        answerList.add("0");
-        answerList.add("1");
-        answerList.add("2");
-        answerList.add("3");
-        answerList.add("4");
+        ListView lvQuestion = findViewById(R.id.lvQuestion);
+        List<String> questionList = new ArrayList<>();
+        questionList.add("Q1");
+        questionList.add("Q2");
+        questionList.add("Q3");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, answerList);
-        lvAnswer.setAdapter(adapter);
-        lvAnswer.setOnItemClickListener(new ListItemClickListener());
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, questionList);
+        lvQuestion.setAdapter(adapter);
+
+//        リストタップのリスナクラスの登録
+        lvQuestion.setOnItemClickListener(new ListItemClickListener());
     }
 
     private class ListItemClickListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            AnswerConfirmDialogFragment dialogFragment = new AnswerConfirmDialogFragment();
-            dialogFragment.show(getSupportFragmentManager(), "AnswerConfirmDialogFragment");
+            Intent intent = new Intent(MainActivity.this, Question.class);
+            intent.putExtra("questionNum", position);
+            startActivity(intent);
         }
     }
 }
---
+++ app/src/main/java/com/websarva/wings/android/quiz_test2/Question.java
@@ -0,0 +1,73 @@
+package com.websarva.wings.android.quiz_test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        //インテントオブジェクト取得
+        Intent intent = getIntent();
+
+        //questionリストから渡された問題番号を取得
+        int questionNum = intent.getIntExtra("questionNum", 0);
+        displayQuestion(questionNum);
+    }
+
+    private void displayQuestion(int num) {
+        TextView tvQuestion = findViewById(R.id.tvQuestion);
+        ListView lvAnswer = findViewById(R.id.lvAnswer);
+        List<String> answerList = new ArrayList<>();
+
+        switch(num) {
+            case 0:
+                tvQuestion.setText("Q1：1+1 = ?");
+                answerList.add("0");
+                answerList.add("1");
+                answerList.add("2");
+                answerList.add("3");
+                answerList.add("4");
+                answerList.add("etc");
+                break;
+
+            case 1:
+                tvQuestion.setText("Q2：9 × 8 = ?");
+                answerList.add("63");
+                answerList.add("70");
+                answerList.add("72");
+                answerList.add("79");
+                answerList.add("81");
+                answerList.add("etc");
+                break;
+
+            case 2:
+                tvQuestion.setText("Q3：900 × 213 × 665 × 0 = ?");
+                answerList.add("123456");
+                answerList.add("984245");
+                answerList.add("94658");
+                answerList.add("457209");
+                answerList.add("0");
+                answerList.add("etc");
+                break;
+
+            default:
+                tvQuestion.setText("Error");
+                break;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(Question.this, android.R.layout.simple_list_item_1, answerList);
+        lvAnswer.setAdapter(adapter);
+    }
+}
