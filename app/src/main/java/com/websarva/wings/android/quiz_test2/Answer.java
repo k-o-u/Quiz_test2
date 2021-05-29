14d8192a52e7a2811086db8cf8a38b905946f618
k_t <ktwmekcm@gmail.com>
2021-05-29 15:27:24 +0900

    modified Answer.java

--- app/src/main/java/com/websarva/wings/android/quiz_test2/Answer.java
+++ app/src/main/java/com/websarva/wings/android/quiz_test2/Answer.java
@@ -11,6 +11,8 @@ public class Answer extends AppCompatActivity {
     private int questionNum;
     private int answerNum;
     private String subject = "";
+    private String rightMsg = "正解です！";
+    private String wrongMsg = "外れです";
 
     private final int[] MATH_ANSWER_LIST = {2, 2, 4};
     private final int[] ENGLISH_ANSWER_LIST = {0, 3, 4};
@@ -28,11 +30,27 @@ public class Answer extends AppCompatActivity {
         answerNum = intent.getIntExtra("answerNum", -1);
         subject = intent.getStringExtra("subject");
 
-        TextView tv = findViewById(R.id.judge);
-        tv.setText(subject);
+        judgeAnswer(questionNum, answerNum, subject);
     }
 
     private void judgeAnswer(int questionNum, int answerNum, String subject) {
+        TextView tv = findViewById(R.id.judge);
 
+        switch (subject) {
+            case "数学":
+                if (answerNum == MATH_ANSWER_LIST[questionNum]) {
+                    tv.setText(rightMsg);
+                } else {
+                    tv.setText(wrongMsg);
+                }
+                break;
+            case "英語":
+                if (answerNum == ENGLISH_ANSWER_LIST[questionNum]) {
+                    tv.setText(rightMsg);
+                } else {
+                    tv.setText(wrongMsg);
+                }
+                break;
+        }
     }
 }
